2ac20b903a3d1ff8790fe6534668cf4e95bfbb4e
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2026-05-31 17:48:01 +0500

    build files chunked

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1100,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // antd va ikonkalarni BIRGA
+          if (
+            id.includes("antd") ||
+            id.includes("rc-") ||
+            id.includes("@ant-design")
+          )
+            return "antd";
+
+          if (id.includes("@tanstack")) return "query";
+          if (id.includes("zustand")) return "store";
+          if (id.includes("react-router")) return "router";
+          if (id.includes("leaflet")) return "map";
+          if (id.includes("xlsx")) return "excel";
+          if (id.includes("socket.io")) return "socket";
+          if (id.includes("i18next") || id.includes("react-i18next"))
+            return "i18n";
+        },
+      },
+    },
+  },
+});
